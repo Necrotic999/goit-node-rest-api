05ec2ceadaabcbd9d95374772764ab78613679e2
Necrotic999 <l.ilya041105@gmail.com>
2024-06-07 14:09:59 +0300

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -21,6 +22,17 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Illia:DyWBI5BAhlxSsT0A@cluster0.f5qanno.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
