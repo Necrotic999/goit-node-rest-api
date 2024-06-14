393fdedf7fa3313d5cbc49fced75434cb84f0dc9
Necrotic999 <l.ilya041105@gmail.com>
2024-06-14 12:24:15 +0300

    update

---
+++ models/usersMongooseSchemas.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+import { emailRegexp } from "../constants/user-constants.js";
+
+const c = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      match: emailRegexp,
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false }
+);
+
+const User = model("user", usersSchema);
+
+export default User;
